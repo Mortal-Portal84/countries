d1fec584157c728a1be0d64cda1b574989323227
Kishor Shachinandan <kshachinandan@anromsocial.com>
2023-02-23 18:00:50 +0200

    1.1.0

--- src/dom-elements.ts
+++ src/dom-elements.ts
@@ -1,48 +1,111 @@
+import {Modal} from 'bootstrap'
 import { Country } from './types'
+import { getCountryById, getCountryByName } from './api-data'
 
-let number = 1
+const searchInput = document.getElementById('input') as HTMLInputElement
+const searchForm = document.getElementById('form') as HTMLFormElement
+const tableBody = document.getElementById('tableBody') as HTMLTableCellElement
+const notFound = document.querySelector('.not-found') as HTMLTableRowElement
+const modalWindow = document.getElementById('modal') as HTMLDivElement
+const modalTitle = modalWindow.querySelector('.country-title') as HTMLHeadingElement
+const modalBody = modalWindow.querySelector('.modal-body') as HTMLDivElement
+const modal = new Modal(modalWindow)
+searchForm.addEventListener('submit', (event) => {
+  event.preventDefault()
 
-const cardTable = document.getElementById('card-table') as HTMLTableCellElement
-const card = document.getElementById('card') as HTMLDivElement
-const tableButton = document.getElementById('card-btn') as HTMLButtonElement
-tableButton.addEventListener('click', () => card.classList.add('hidden'))
+  notFound.classList.add('hidden')
 
-export const createCountryCard = () => {
-  const tableBodyRow = document.createElement('tr') as HTMLTableRowElement
-  const tableNumber = document.createElement('th') as HTMLTableCellElement
-        tableNumber.scope = 'row'
-  const tableInfo = document.createElement('td') as HTMLTableCellElement
-  const tableDescription = document.createElement('td') as HTMLTableCellElement
+  getCountryByName(searchInput.value)
+    .then((countries) => {
+      if (countries?.length) {
+        updateTable(countries)
+      } else {
+        tableBody.replaceChildren()
+        notFound.classList.remove('hidden')
+      }
+    })
+})
 
-  tableNumber.textContent = `${number}`
-  tableInfo.textContent = 'Hare Krishna!'
-  tableDescription.textContent = 'Hare Rama!'
-  tableBodyRow.append(tableNumber, tableInfo, tableDescription)
-  cardTable.append(tableBodyRow)
+export const createCountryCard = (country: Country): HTMLDivElement => {
+  const card = document.createElement('div')
+  card.classList.add('card')
 
-  return cardTable
+  const img = document.createElement('img')
+  img.classList.add('card-img-top')
+  img.src = country.flags.svg
+  img.alt = country.name.common
+
+  const body = document.createElement('div')
+  body.classList.add('card-body')
+
+  const p = document.createElement('p')
+  p.classList.add('card-text')
+  p.textContent = `Capital: ${country.capital}`
+
+  body.append(p)
+
+  const ul = document.createElement('ul')
+  ul.classList.add('list-group', 'list-group-flush')
+
+  const list: HTMLLIElement[] = country.timezones.map((timezone) => {
+    const li = document.createElement('li')
+    li.classList.add('list-group-item')
+    li.textContent = timezone
+
+    return li
+  })
+
+  ul.append(...list)
+
+  const body2 = document.createElement('div')
+  body2.classList.add('card-body')
+
+  const a = document.createElement('a') as HTMLAnchorElement
+  a.href = country.maps.googleMaps
+  a.textContent = 'Map'
+  a.target = '_blank'
+
+  body2.append(a)
+
+  card.append(img, body, ul, body2)
+
+  return card
 }
-export const createTableRow = (country: Country): HTMLTableRowElement => {
-  const tabRow = document.createElement('tr') as HTMLTableRowElement
-        tabRow.classList.add('country-row')
-  const tabHeader = document.createElement('th') as HTMLTableCellElement
+
+const openModal = async (id: string): Promise<void> => {
+  getCountryById(id).then((countries) => {
+    const [country] = countries
+    modalTitle.textContent = country.name.official
+    const card = createCountryCard(country)
+    modalBody.replaceChildren(card)
+    modal.show()
+  })
+}
+
+export const createTableRow = (country: Country, index: number): HTMLTableRowElement => {
+  const row = document.createElement('tr') as HTMLTableRowElement
+  row.classList.add('country-row')
+  const header = document.createElement('th') as HTMLTableCellElement
   const countryName = document.createElement('td') as HTMLTableCellElement
   const capital = document.createElement('td') as HTMLTableCellElement
   const flag = document.createElement('td') as HTMLTableCellElement
 
-  tabHeader.scope = 'row'
-  tabHeader.textContent = `${number}`
+  header.scope = 'row'
+  header.textContent = `${index}`
   countryName.textContent = country.name['common']
   capital.textContent = country.capital ? country.capital[0] : ''
   flag.textContent = country.flag
-  number++
 
-  tabRow.append(tabHeader, countryName, capital, flag)
-  tabRow.addEventListener('click', () => {
-    cardTable.innerHTML = ''
-    createCountryCard()
-    card.classList.remove('hidden')
+  row.append(header, countryName, capital, flag)
+  row.addEventListener('click', async () => {
+    await openModal(country.name.common)
   })
 
-  return tabRow
+  return row
+}
+
+export const updateTable = (countries: Country[]): void => {
+  tableBody
+    .replaceChildren(...countries
+      ?.map((country, index) => createTableRow(country, index + 1)))
 }
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,10 @@
 // @ts-ignore
 import * as bootstrap from 'bootstrap'
-import { getCountriesList, getCountryByName } from './api-data'
-import { createTableRow } from './dom-elements'
-import { Country } from './types'
+import { getCountriesList } from './api-data'
+import { updateTable } from './dom-elements'
+
 import './style.scss'
 
-const tableBody = document.getElementById('tableBody') as HTMLTableCellElement
-const searchInput = document.getElementById('input') as HTMLInputElement
-const searchForm = document.getElementById('form') as HTMLFormElement
-const notFound = document.querySelector('.not-found') as HTMLTableRowElement
+getCountriesList()
+  .then((countries) => updateTable(countries))
 
-getCountriesList().then(result => tableBody.append(...result?.map((country: Country) => createTableRow(country))))
-
-searchForm.addEventListener('submit',(event) => {
-  event.preventDefault()
-  notFound.classList.add('hidden')
-  tableBody.innerHTML = ''
-    getCountryByName(searchInput.value)
-      .then(response => response?.length
-        ? tableBody.append(...response.map((country) => createTableRow(country)))
-        : notFound.classList.remove('hidden'))
-})
-
-getCountryByName('Belarus').then(result => console.log(result))
